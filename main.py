0cd5921fe659a398f961c5106419b3f7704d6d1f
Mark Cyster <mcyster@gmail.com>
2020-03-25 01:51:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import turtle
+
+pen = turtle.Turtle()
+pen.up()
+pen.backward(100)
+pen.down()
+
+
+def flake_side(angle, length):
+  pen.left(angle)
+  pen.forward(length)
+  if length > 1:
+    flake_point(angle, length / 3)
+  else:
+    pen.forward(length)
+  pen.forward(length)
+  pen.right(angle)
+
+
+def flake_point(angle, length):
+  flake_side(angle, length)
+  pen.right(180 - angle)
+  flake_side(angle, length)
+  pen.right(180 + angle)
+
+for i in xrange(4):
+  flake_point(60, 90)
+  pen.right(90)
